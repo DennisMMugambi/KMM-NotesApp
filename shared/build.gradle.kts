7d835ee086af270065d7cea7f8a5df9c12ac162c
Dennis Mugambi <dennismugambi@Denniss-MacBook-Pro.local>
2023-12-18 16:41:22 +0300

    initializes repository

---
+++ shared/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+            dependencies {
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+            }
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.example.noteappkmm"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
